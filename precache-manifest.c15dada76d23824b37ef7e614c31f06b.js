99267aa88590973c57ba40965cc52f1e6c9f13f9
Jorge Fuente-Alba <AsPerSpecUnity@gmail.com>
2020-10-03 21:29:37 -0300

    Updates

---
+++ precache-manifest.c15dada76d23824b37ef7e614c31f06b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d4d5826e5615891544fd1a23c9f340",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f27c8701034d5d180d8",
+    "url": "/static/css/main.0ab83a3b.chunk.css"
+  },
+  {
+    "revision": "8b44078896e6bc3f423d",
+    "url": "/static/js/2.56829af2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56829af2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f27c8701034d5d180d8",
+    "url": "/static/js/main.8b185a51.chunk.js"
+  },
+  {
+    "revision": "a083e7f0ece0d8980ea2",
+    "url": "/static/js/runtime-main.164668f1.js"
+  }
+]);
