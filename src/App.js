b5d5f47f11cc9a8c527ec81cf07ee592718c90dd
Jorge Fuente-Alba <AsPerSpecUnity@gmail.com>
2020-10-16 03:12:31 -0300

    Implemented all copy/fix options and new ones

--- src/App.js
+++ src/App.js
@@ -23,14 +23,16 @@ export default ()=>{
   const [coreSourceOnFix,setCoreSourceOnFix] = useState("detect");
   const [pickOnlyOnFixType,setPickOnlyTypeOnFix] = useState("label");
   const [pickOnlyOnFixSpec,setPickOnlyOnFixSpec] = useState("");
+  const [deduceLabelOnFix,setDeduceLabelOnFix] = useState(false);
   const [appendToLabelOnFixText,setAppendToLabelOnFixText] = useState("(fixed)");
   const [changeBasePathOnFixPath,setChangeBasePathOnFixPath] = useState("C:\\");
+  const [matchTypeOnFix,setMatchTypeOnFix] = useState("crc");
   const [copyOptsVisible,setCopyOptsVisible] = useState(false);
   const [fixOptsVisible,setFixOptsVisible] = useState(false);
   
   const copyOnClick = (gameData,index)=>{
     if (targetController.playlist) {
-      targetController.add(gameData);
+      copyWithOptions(gameData,sourceController,targetController);
       targetController.filter(filterType, filter);
     }
   }
@@ -38,10 +40,45 @@ export default ()=>{
   const copyAll = ()=>{
     if (sourceController.playlist) {
       sourceController.playlist.items.forEach(gameData => {
-        targetController.add(gameData);
+        copyWithOptions(gameData,sourceController,targetController);
       });
       targetController.filter(filterType, filter);
     }
+  }
+
+  const copyWithOptions = (gameData,source,target)=>{
+    const newGameData = {...gameData};
+    if (changeCoreOnCopy) {
+      switch (coreSourceOnCopy) {
+        case "sourcePlaylist":
+          newGameData.core_path = source.playlist.default_core_path;
+          newGameData.core_name = source.playlist.default_core_name;
+          break;
+        case "targetPlaylist":
+          newGameData.core_path = target.playlist.default_core_path;
+          newGameData.core_name = target.playlist.default_core_name;
+          break;
+        case "detect":
+          newGameData.core_path = "DETECT";
+          newGameData.core_name = "DETECT";
+          break;
+        default:
+          break;
+      }
+    }
+    if (deduceLabelOnCopy && gameData.path && (gameData.label === labelCheckOnCopy)) {
+      const matches = gameData.path.match(/([^\\#]+)\....$/);
+      if (matches.length>1) {
+        newGameData.label = matches[1];
+      }
+    }
+    if (changeBasePathOnCopy && gameData.path) {
+      const matches = gameData.path.match(/([^\\]+\....)$/);
+      if (matches.length>1) {
+        newGameData.path = `${basePathOnCopy}${matches[1]}`;
+      }
+    }
+    target.add(newGameData);
   }
 
   const deleteOnClick = (gameData,index)=>{
@@ -120,12 +157,20 @@ export default ()=>{
     setPickOnlyOnFixSpec(newPickOnlyOnFixSpec.target.value);
   }
 
+  const onChangeDeduceLabelOnFix = (newDeduceLabelOnFix)=>{
+    setDeduceLabelOnFix(newDeduceLabelOnFix.target.checked);
+  }
+
   const onChangeAppendToLabelOnFixText = (newAppendToLabelOnFixText)=>{
     setAppendToLabelOnFixText(newAppendToLabelOnFixText.target.value);
   }
 
   const onChangeChangeBasePathOnFixPath = (newChangeBasePathOnFixPath)=>{
     setChangeBasePathOnFixPath(newChangeBasePathOnFixPath);
+  }
+
+  const onChangeMatchTypeOnFix = (newMatchTypeOnFix)=>{
+    setMatchTypeOnFix(newMatchTypeOnFix.target.value);
   }
 
   const onClickCopyOpts = ()=>{
@@ -146,33 +191,32 @@ export default ()=>{
   }
 
   const fixAll = ()=>{
-    const fixType="crc32";
     let list;
-    switch (fixType) {
+    switch (matchTypeOnFix) {
       case "label":
-        list = targetController.filteredItems.map(wrap=>wrap.game.label);
+        list = targetController.filteredItems.filter(wrap=>(!pickOnlyOnFix || (findLeftSideForPick(wrap.game,pickOnlyOnFixType)===pickOnlyOnFixSpec))).map(wrap=>wrap.game.label);
         break;
       case "filename":
-        list = targetController.filteredItems.map(wrap=>wrap.game.path.match(/([^\\#]+\....)$/)[1]);
+        list = targetController.filteredItems.filter(wrap=>(!pickOnlyOnFix || (findLeftSideForPick(wrap.game,pickOnlyOnFixType)===pickOnlyOnFixSpec))).map(wrap=>wrap.game.path.match(/([^\\#]+\....)$/)[1]);
         break;
-      case "crc32":
-        list = targetController.filteredItems.map(wrap=>wrap.game.crc32);
+      case "crc":
+        list = targetController.filteredItems.filter(wrap=>(!pickOnlyOnFix || (findLeftSideForPick(wrap.game,pickOnlyOnFixType)===pickOnlyOnFixSpec))).map(wrap=>wrap.game.crc32);
         break;
-      case "path":
-        list = targetController.filteredItems.map(wrap=>wrap.game.path);
+      case "fullpath":
+        list = targetController.filteredItems.filter(wrap=>(!pickOnlyOnFix || (findLeftSideForPick(wrap.game,pickOnlyOnFixType)===pickOnlyOnFixSpec))).map(wrap=>wrap.game.path);
         break;
       default:
         list = [];
         break;
     }
     list.forEach(i=>{
-      switch (fixType) {
+      switch (matchTypeOnFix) {
         case "label":
           if (sourceController.fullLabel[i] && sourceController.fullLabel[i].length===1) {
             while (targetController.fullLabel[i] && targetController.fullLabel[i].length>0) {
               targetController.delete(targetController.fullLabel[i][0].index);
             }
-            targetController.add(sourceController.fullLabel[i][0].game);
+            targetController.add(applyFixChanges(sourceController.fullLabel[i][0].game),sourceController,targetController);
           }
           break;
         case "filename":
@@ -180,23 +224,23 @@ export default ()=>{
             while (targetController.fullFilename[i] && targetController.fullFilename[i].length>0) {
               targetController.delete(targetController.fullFilename[i][0].index);
             }
-            targetController.add(sourceController.fullFilename[i][0].game);
+            targetController.add(applyFixChanges(sourceController.fullFilename[i][0].game,sourceController,targetController));
           }
           break;
-        case "crc32":
+        case "crc":
           if (sourceController.fullCrc[i] && sourceController.fullCrc[i].length===1) {
             while (targetController.fullCrc[i] && targetController.fullCrc[i].length>0) {
               targetController.delete(targetController.fullCrc[i][0].index);
             }
-            targetController.add(sourceController.fullCrc[i][0].game);
+            targetController.add(applyFixChanges(sourceController.fullCrc[i][0].game,sourceController,targetController));
           }
           break;
-        case "path":
+        case "fullpath":
           if (sourceController.fullPath[i] && sourceController.fullPath[i].length===1) {
             while (targetController.fullPath[i] && targetController.fullPath[i].length>0) {
               targetController.delete(targetController.fullPath[i][0].index);
             }
-            targetController.add(sourceController.fullPath[i][0].game);
+            targetController.add(applyFixChanges(sourceController.fullPath[i][0].game,sourceController,targetController));            
           }
           break;
         default:
@@ -205,10 +249,72 @@ export default ()=>{
     });
   }
 
+  const findLeftSideForPick = (gameData,type)=>{
+    let leftSide = "";
+    switch (type) {
+      case "label":
+        leftSide = gameData.label;
+        break;
+      case "crc":
+        leftSide = gameData.crc32;
+        break;
+      case "basePath":
+        if (gameData.path) {
+          const match = gameData.path.match(/(.+?)[^\\]+\....$/);
+          if (match.length>1) {
+            leftSide = match[1];
+          }
+        }
+        break;
+      default:
+        break;
+    }
+    return leftSide;
+  }
+
+  const applyFixChanges = (gameData,source,target)=>{
+    const newGameData = {...gameData};
+    if (changeCoreOnFix) {
+      switch (coreSourceOnFix) {
+        case "sourcePlaylist":
+          newGameData.core_path = source.playlist.default_core_path;
+          newGameData.core_name = source.playlist.default_core_name;
+          break;
+        case "targetPlaylist":
+          newGameData.core_path = target.playlist.default_core_path;
+          newGameData.core_name = target.playlist.default_core_name;
+          break;
+        case "detect":
+          newGameData.core_path = "DETECT";
+          newGameData.core_name = "DETECT";
+          break;
+        default:
+          break;
+      }
+    }
+    if (deduceLabelOnFix && newGameData.path) {
+      const matches = gameData.path.match(/([^\\#]+)\....$/);
+      if (matches.length>1) {
+        newGameData.label = matches[1];
+      }  
+    }
+    if (appendToLabelOnFix) {
+      newGameData.label = `${newGameData.label}${appendToLabelOnFixText}`;
+    }
+    if (changeBasePathOnFix && newGameData.path) {
+      const matches = newGameData.path.match(/([^\\]+\....)$/);
+      if (matches.length>1) {
+        newGameData.path = `${changeBasePathOnFixPath}${matches[1]}`;
+      }
+    }
+
+    return newGameData;
+  }
+
   const targetButtons={
     Reset: targetController.empty.bind(targetController),
     "New": newTargetPlaylist,
-    "Fix visible by CRC": fixAll,
+    "Fix visible": fixAll,
     Export: targetController.exportPlaylist.bind(targetController),
   }
 
@@ -247,6 +353,14 @@ export default ()=>{
         {
           fixOptsVisible?(
             <ul>
+              <li><input type="checkbox" checked disabled />
+                Match to source on <select value={matchTypeOnFix} onChange={onChangeMatchTypeOnFix}>
+                  <option value="label">Label</option>
+                  <option value="crc">CRC</option>
+                  <option value="filename">Filename</option>
+                  <option value="fullpath">Full path</option>
+                </select>
+              </li>
               <li><input type="checkbox" checked={changeCoreOnFix} onChange={onChangeChangeCoreOnFix} />
                 Change core name/path to <select value={coreSourceOnFix} onChange={onChangeCoreSourceOnFix}>
                   <option value="sourcePlaylist">Source Playlist default core</option>
@@ -260,6 +374,9 @@ export default ()=>{
                   <option value="crc">crc</option>
                   <option value="basePath">base path</option>
                 </select> matching <input type="text" value={pickOnlyOnFixSpec} onChange={onChangePickOnlyOnFixSpec}/>
+              </li>
+              <li><input type="checkbox" checked={deduceLabelOnFix} onChange={onChangeDeduceLabelOnFix} />
+                Deduce label from filename
               </li>
               <li><input type="checkbox" checked={appendToLabelOnFix} onChange={onChangeAppendToLabelOnFix} />
                 Append <input type="text" value={appendToLabelOnFixText} onChange={onChangeAppendToLabelOnFixText}/> to label
